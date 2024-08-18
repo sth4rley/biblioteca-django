f39ab1e0788a595a24636b1494ef87db514e4c1f
Stharley <sstharley@gmail.com>
2024-08-18 08:26:59 -0300

    add DRF

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core.apps.CoreConfig'
+    'core.apps.CoreConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
