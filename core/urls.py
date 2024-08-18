192daa11533f39149abfe69652b23b05b14cb1cf
Stharley <sstharley@gmail.com>
2024-08-17 22:41:57 -0300

    Initial commit

---
+++ core/urls.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('livros/', views.livro_list_create, name='livros-list-create'),
+    path('livros/<int:pk>/', views.livro_detail, name='livro-detail'),
+]
